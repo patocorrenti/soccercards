d58b29936b51c61c96dfa6d5edff9e8393158df1
Pato Correnti <patocorrenti@gmail.com>
2021-12-05 23:22:32 -0300

    add: card team colors

--- components/cards/list/Card.js
+++ components/cards/list/Card.js
@@ -6,16 +6,20 @@ import Styles from './Card.module.scss'
 
 export default function SoccerCard ({ card, id }) {
 
-  const cardLink = `/card/${id}`;
+  const cardLink = `/card/${id}`
+  const primaryColor = { backgroundColor: `#${card.team.color_1 || 'fff'}` }
+  const secondaryColor = { backgroundColor: `#${card.team.color_2 || 'fff'}` }
 
   return (
     <article className={Styles.card}>
+      <div className={Styles.image}>
+        <div className={Styles.colorsec} style={secondaryColor}></div>
+        <a href={cardLink}>
+          IMAGE
+        </a>
+        <div className={Styles.colorpri} style={primaryColor}></div>
+      </div>
       <div className={Styles.data}>
-        <div>
-          <a href={cardLink}>
-            IMAGE
-          </a>
-        </div>
         <header className={Styles.header}>
           <h2>
             <Link href={cardLink}>

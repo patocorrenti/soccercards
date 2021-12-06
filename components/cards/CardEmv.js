4949ec26cd2b38ebdfe4b3b4e8f7c5a1771a92ef
Pato Correnti <patocorrenti@gmail.com>
2021-12-05 22:41:52 -0300

    add: estimated market value style

--- components/cards/CardEmv.js
+++ components/cards/CardEmv.js
@@ -1,15 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEthereum } from '@fortawesome/free-brands-svg-icons';
+import { faLongArrowAltUp, faLongArrowAltDown } from '@fortawesome/free-solid-svg-icons';
+
 import Styles from './CardEmv.module.scss'
 
 export default function CardEmv ({ emv }) {
+
+  const evoStyle = getEvolutionStyle( emv.evolution );
+
   return (
     <div className={Styles.emv}>
       <span className={Styles.legend}>
         EMV
       </span>
-      <span className={Styles.value}>
-        { emv.emv || '???' }
+      <span className={Styles.ethsym}>
+        <FontAwesomeIcon icon={faEthereum} className={Styles.icon} />
       </span>
-      { emv.evolution && emv.evolution }
+      <span className={`${Styles.value} ${evoStyle}`}>
+        <span className={Styles.price}>
+          { emv.emv || '???' }
+        </span>
+        <span className={Styles.evolution}>
+          { emv.evolution === 'up' && <FontAwesomeIcon icon={faLongArrowAltUp} className={Styles.up} /> }
+          { emv.evolution === 'down' && <FontAwesomeIcon icon={faLongArrowAltDown} className={Styles.down} /> }
+        </span>
+      </span>
     </div>
   )
+}
+
+function getEvolutionStyle( evo ) {
+  if ( ! evo ) return '';
+  return evo === 'up' ? Styles.up : Styles.down;
 }
